a2295acce430a7eba196a290842746909ff9d76c
Anais Ahmed <anaismahmed@uchicago.edu>
2019-10-10 16:23:59 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            peopleInSpace: []
+        }
+    }
+
+    componentDidMount(){
+        return fetch('http://api.open-notify.org/astros.json')
+        .then(res => res.json())
+        .then(function(json){
+            this.setState({peopleinSpace: json.people})
+        })
+        .catch(error => console.log(error))
+    }
+
+    render(){
+        return(
+            <div>
+                {this.state.peopleInSpace.map(person => person.name)}
+            </div>
+        )
+    }
+
+
+}
